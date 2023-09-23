8d622b832ce9334e3bece0d032881c6d3cd06538
Charles Lughas <lordlughas@gmail.com>
2023-09-23 20:06:16 +0100

    Create authorization module

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,4 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 """
 API authentication module
 """
@@ -11,22 +11,22 @@ class Auth:
     """ authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    """ check done to know if API requires authenticationi"""
-    if path is None or not exluded_paths:
+        """ check done to know if API requires authenticationi"""
+        if path is None or not exluded_paths:
+            return True
+        for item in exluded_paths:
+            if item.endswith('*') and path.startswith(item[:-1]):
+                return False
         return True
-    for item in exluded_paths:
-        if item.endswith('*') and path.startswith(item[:-1]):
-            return False
-    return True
 
-def authorization_header(self, request=None) -> str:
-    """ Checks if Authorization request header is present
+    def authorization_header(self, request=None) -> str:
+        """ Checks if Authorization request header is present
         & contains values """
-    if request is None or "Authorization" not in request.headers:
+        if request is None or "Authorization" not in request.headers:
             return None
         else:
             return request.headers.get('Authorization')
 
-def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         """ placeholder """
         return None
